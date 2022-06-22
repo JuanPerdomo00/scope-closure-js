f2140cedc1132fe1699774fc70a644d23250774d
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-06-22 16:12:30 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,16 @@
+// console.log(nameDog); // undefinded
+// var nameDog = "Lulu";
+
+nameDog();
+
+function nameDog() {
+  var dog = "lulu";
+  console.log(`mi perra ${dog} es hermosa`);
+}
+
+// var dog = "lulu";
+
+nameOfDog("Elmo");
+function nameOfDog(name) {
+  console.log(name);
+}
